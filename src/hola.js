437bb15d9deb4b624387e5467295fcb0354e599c
Jonatan <alvarez.geek@hotmail.com>
2021-07-31 01:20:49 -0500

    Curso typescript-EdTeam

---
+++ src/hola.js
@@ -0,0 +1,7 @@
+"use strict";
+var saludo = 'Bienvenidos a Typescript';
+console.log(saludo);
+var fecha = '30/07/2021';
+console.log('fecha', fecha);
+var edad = 25;
+console.log('edad', edad);
---
+++ src/hola.ts
@@ -0,0 +1,8 @@
+let saludo = 'Bienvenidos a Typescript';
+console.log(saludo);
+
+let fecha = '30/07/2021';
+console.log('fecha', fecha);
+
+let edad = 25;
+console.log('edad', edad);
